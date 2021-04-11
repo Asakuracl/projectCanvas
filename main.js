6d3db468844d9534aaaa6a5659578892b5a076d5
asakuracl <champi.ludo@hotmail.fr>
2021-04-11 18:57:22 -0300

    replace triangle by arrow

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ class myCanvas {
         this.moveY = Math.floor(Math.random() * canvas.height);
         this.speedX = 1;
         this.speedY = 1;
-        this.triangleLength = 20;
+        this.triangleLength = 0;
+        this.arrowLength = 40;
 
         //listener on load
         window.addEventListener("load", this.animate.bind(this));
@@ -20,20 +21,21 @@ class myCanvas {
         requestAnimationFrame(this.animate.bind(this));
         this.ctx.clearRect(0, 0, canvas.height, canvas.width);
         this.ctx.beginPath();
-        this.ctx.moveTo(0 + this.moveX, 10 + this.moveY);
-        this.ctx.lineTo(20 + this.moveX, 0 + this.moveY);
-        this.ctx.lineTo(20 + this.moveX, 20 + this.moveY);
-        this.ctx.closePath()
-        this.ctx.lineWidth = 1;
-        this.ctx.strokeStyle = '#0f4c81';
+        this.ctx.moveTo(21 + this.moveX, 41 + this.moveY);
+        this.ctx.lineTo(41 + this.moveX, 40 + this.moveY);
+    
+        this.ctx.moveTo(40 + this.moveX, 38+ this.moveY);
+        this.ctx.lineTo(37+ this.moveX, 60+ this.moveY);
+        this.ctx.lineWidth = 4;
+ 
+        this.ctx.moveTo(33 + this.moveX, 46 + this.moveY);
+        this.ctx.quadraticCurveTo(11 + this.moveX, 60+ this.moveY, 1 + this.moveX, 85 + this.moveY);
+        this.ctx.lineWidth = 3;
+        this.ctx.strokeStyle = 'red';
         this.ctx.stroke();
 
-        (this.moveX + this.triangleLength > canvas.width || this.moveX < 0) ? this.speedX = -this.speedX: this.speedX;
-        (this.moveY + this.triangleLength > canvas.height || this.moveY < 0) ? this.speedY = -this.speedY : this.speedY;
-   
+        (this.moveX + this.arrowLength > canvas.width || this.moveX < 0) ? this.speedX = -this.speedX: this.speedX;
         this.moveX += this.speedX;
-        this.moveY += this.speedY;
-
     }
 
 }
